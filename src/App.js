782b5e6f04bf40fcbde615963465561add99e3bc
chaitanms <chaitanms@gmail.com>
2021-05-06 23:21:00 +0530

    Day 3 click Hanlder and value passing to the parent

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Expenses from './components/Expenses/Expenses'
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
 
@@ -25,9 +26,14 @@ const App = () => {
     }
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log('inside the App.js');
+    console.log(expense);
+  }
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses}> </Expenses>
     </div>
   );
